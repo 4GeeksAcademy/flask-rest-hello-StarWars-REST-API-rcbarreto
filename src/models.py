80a0c5ae724fbc6b4bc448d32c7cb93a798a0f6c
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-12-01 18:18:51 +0000

    Revert "DB"
    This reverts commit ddfc69139c712885d17008720760d40d77e57924.

--- src/models.py
+++ src/models.py
@@ -3,74 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "is_active": self.is_active
-        }
-
-class Planet(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.String(800))
-    weather = db.Column(db.String(250))
-    population = db.Column(db.String(250))
-
-    def __repr__(self):
-        return '<Planet %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "weather": self.weather,
-            "population": self.population
-        }
-
-class People(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    origin_planet = db.Column(db.String(250), nullable=False)
-    eye_color = db.Column(db.String(250), nullable=False)
-    hair_color = db.Column(db.String(250), nullable=False)
-
-    def __repr__(self):
-        return '<People %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            
-        }
-
-class Favorite(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    character_id = db.Column(db.Integer, db.ForeignKey('people.id'))
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
-
-    def __repr__(self):
-        return '<Favorite %r>' % self.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "character_id": self.character_id,
-            "planet_id": self.planet_id
+            # do not serialize the password, its a security breach
         }
